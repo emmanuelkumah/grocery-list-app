e65d3bfaffebe36bc43fa4dfb9ee7c8ac1dda2f8
EmmanuelKumah <mailkumah@gmail.com>
2021-04-14 10:16:39 +0000

    Added routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Cards from "./components/Cards/Cards";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/Navigation/NavBar";
 import Home from "./components/pages/Home/Home";
 import SignUp from "./components/SignUp/SignUp";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" exact>
             <Home />
           </Route>
+          <Route path="/testimonials" component={Testimonials} />
+          <Route path="/about" component={Cards} />
           <Route path="/sign-up">
             <SignUp />
           </Route>
